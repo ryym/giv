278f1420727e84f6f7b28689bcb942d60536cdbe
ryym <ryym.64@gmail.com>
2017-03-19 22:06:45 +0900

    Add TODO about security
    Disabling `nodeIntegration` in the renderer process
    makes the app more secure, but it means we can't use
    Node functionalities in the process.

--- main/index.js
+++ main/index.js
@@ -1,5 +1,9 @@
 const { app, BrowserWindow } = require('electron');
 
+// TODO: What about security?
+// https://electron.atom.io/docs/tutorial/security/
+// http://utf-8.jp/public/2016/0629/electron.pdf
+
 module.exports = function bootstrap() {
   app.on('ready', () => {
     const window = new BrowserWindow({
