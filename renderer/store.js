ca6bbdcbbdb42f37a284b02508526390b86be5ef
ryym <ryym.64@gmail.com>
2017-03-20 20:11:28 +0900

    chore(package): Add redux-logger

--- renderer/store.js
+++ renderer/store.js
@@ -2,16 +2,18 @@ import {
   createStore,
   applyMiddleware,
 } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import createLogger from 'redux-logger';
 import reducers from './reducers';
 import sagas from './sagas';
-import createSagaMiddleware from 'redux-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   reducers,
   applyMiddleware(
-    sagaMiddleware
+    sagaMiddleware,
+    createLogger()
   )
 );
 
