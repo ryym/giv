05b90bad47f6cd7ef37ed70df37f2bea5b0c3e80
ryym <ryym.64@gmail.com>
2017-03-20 20:45:49 +0900

    chore(lint): Fix capital letter rule for Immutable js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   "extends": ["ryym/base-react"],
   "plugins": ["react"],
+  "rules": {
+    "new-cap": [0, {
+      // Exclude Immutable library objects.
+      "capIsNewExceptions": [
+        "List", "Stack",
+        "Map", "OrderedMap",
+        "Set", "OrderedSet",
+        "Record", "Seq", "Collection",
+      ]
+    }]
+  }
 }
