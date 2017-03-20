39a653efaedc695c9f1964eefe48e67d6417b1ef
ryym <ryym.64@gmail.com>
2017-03-20 20:47:35 +0900

    feat(main): Open DevTools under development

--- main/bootstrap.js
+++ main/bootstrap.js
@@ -14,5 +14,9 @@ module.exports = function bootstrap() {
     });
 
     window.loadURL(`file://${__dirname}/index.html`);
+
+    if (process.env.NODE_ENV === 'development') {
+      window.webContents.openDevTools();
+    }
   });
 };
