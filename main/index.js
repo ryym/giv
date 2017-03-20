e45214970786af684ea04b69ebf96e3ebca0b18b
ryym <ryym.64@gmail.com>
2017-03-20 14:23:34 +0900

    Improve development and packaging process
    - Put all distribution files to `dist`
    - Use `electron-packager`

--- main/index.js
+++ main/index.js
@@ -1,18 +1,1 @@
-const { app, BrowserWindow } = require('electron');
-
-// TODO: What about security?
-// https://electron.atom.io/docs/tutorial/security/
-// http://utf-8.jp/public/2016/0629/electron.pdf
-
-module.exports = function bootstrap() {
-  app.on('ready', () => {
-    const window = new BrowserWindow({
-      x: 0,
-      y: 0,
-      width: 1000,
-      height: 1000,
-    });
-
-    window.loadURL(`file://${__dirname}/../index.html`);
-  });
-};
+require('./bootstrap')();
