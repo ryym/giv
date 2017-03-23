eb98a2d5e4f5ea4aa609b3e9f559878427d80dd5
ryym <ryym.64@gmail.com>
2017-03-23 15:50:01 +0900

    fix(rend/logger): Print immutable objects correctly

--- renderer/store.js
+++ renderer/store.js
@@ -1,15 +1,24 @@
-import {
-  createStore,
-  applyMiddleware,
-} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import createLogger from 'redux-logger';
 import { routerMiddleware } from 'react-router-redux';
+import { Iterable } from 'immutable'
 import history from './history';
 import sagas from './sagas';
 import createReader from './state/reader';
 import createReducer from './state/reducer';
 import createReadableStore from './redux/readable-store';
+
+const configureLogger = () => {
+  const stateTransformer = state => Object.keys(state).reduce((snap, name) => {
+    const slice = state[name]
+    snap[name] = Iterable.isIterable(slice) ? slice.toJS() : slice;
+    return snap
+  }, {})
+  return createLogger({
+    stateTransformer,
+  })
+}
 
 export default function configureStore() {
   const sagaMiddleware = createSagaMiddleware();
@@ -18,7 +27,7 @@ export default function configureStore() {
     createReducer(),
     applyMiddleware(
       sagaMiddleware,
-      createLogger(),
+      configureLogger(),
 
       // Apply router middleware after the logger
       // to log router actions.
