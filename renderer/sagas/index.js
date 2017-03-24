50c595a14e0527f445ba3662e63f7d6763614a45
ryym <ryym.64@gmail.com>
2017-03-24 22:21:37 +0900

    refac(rend/saga): Move user config saga to another file

--- renderer/sagas/index.js
+++ renderer/sagas/index.js
@@ -1,35 +1,15 @@
+import { fork, put, takeEvery } from 'redux-saga/effects';
+import userConfigSaga from './userConfig';
 import {
-  fork, take, takeEvery,
-  put, call,
-} from 'redux-saga/effects';
-import {
-  UPDATE_TOKEN,
   FETCH_NOTIFS_START,
   fetchNotifsSuccess,
-  LOAD_USER_CONFIG,
-  loadUserConfigSuccess,
 } from '../actions';
-import {
-  sendNewToken,
-  fetchUserConfig,
-} from '../lib/ipc';
-
-function* loadUserConfig() {
-  yield take(LOAD_USER_CONFIG);
-  const config = yield call(fetchUserConfig);
-  yield put(loadUserConfigSuccess(config));
-}
-
-function* saveAccessToken(action) {
-  yield call(sendNewToken, action.payload.token);
-}
 
 function* fetchNotifications() {
   yield put(fetchNotifsSuccess([]));
 }
 
 export default function* rootSaga() {
-  yield fork(loadUserConfig);
+  yield fork(userConfigSaga);
   yield takeEvery(FETCH_NOTIFS_START, fetchNotifications);
-  yield takeEvery(UPDATE_TOKEN, saveAccessToken);
 }
