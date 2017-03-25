8c226d5f5ea20a12444e6042e601b1d110152cdb
ryym <ryym.64@gmail.com>
2017-03-25 16:43:39 +0900

    fix(rend/notifs): Prevent rendering notifications without access token

--- renderer/components/Notifications/index.js
+++ renderer/components/Notifications/index.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { connectWithReader } from '../../redux';
+import { push } from '../../actions';
 
-const Notifications = ({ notifs = [] }) => {
-  return (
-    <div>
-      <h1>notifications</h1>
-      {
-        notifs.map(nt => (
-          <div key={nt.id}>{nt.subject.title}</div>
-        ))
-      }
-    </div>
-  );
-};
+class Notifications extends React.Component {
+  componentDidMount() {
+    const { hasAccessToken, dispatch } = this.props;
+    if (!hasAccessToken) {
+      dispatch(push('/access-token'));
+    }
+  }
+
+  render() {
+    const { notifs = [] } = this.props;
+    return (
+      <div>
+        <h1>notifications</h1>
+        {
+          notifs.map(nt => (
+            <div key={nt.id}>{nt.subject.title}</div>
+          ))
+        }
+      </div>
+    );
+  }
+}
 
 export default connectWithReader(
-  ({ pagination }) => ({
+  ({ userConfig, pagination }) => ({
+    hasAccessToken: Boolean(userConfig.accessToken),
     notifs: pagination.unreadNotifications,
   })
 )(Notifications);
