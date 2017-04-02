3e8830233c823dc14ba818725ab6aeab8922b33c
ryym <ryym.64@gmail.com>
2017-04-02 20:59:47 +0900

    feat(rend/async): Add util to run promises concurrently within limit

---
+++ renderer/lib/async/run-with-limit.js
@@ -0,0 +1,35 @@
+/**
+ * Run multiple async operations concurrently
+ * within the specified max running count.
+ * (XXX: There may be more better implementation?)
+ */
+export default function runWithLimit(max = 1, {
+  nextTick = f => setTimeout(f, 50),
+} = {}) {
+  let nSpaces = max;
+
+  const waitIfNecessary = () => new Promise(resolve => {
+    const check = () => {
+      if (nSpaces > 0) {
+        nSpaces -= 1;
+        return resolve();
+      }
+      nextTick(check);
+    };
+    check();
+  });
+
+  return function waitAndRun(makePromise) {
+    return waitIfNecessary().then(() => {
+      return makePromise()
+        .then(value => {
+          nSpaces += 1;
+          return value;
+        })
+        .catch(err => {
+          nSpaces += 1;
+          throw err;
+        });
+    });
+  };
+}
