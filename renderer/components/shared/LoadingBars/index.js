51c5f8c036ae30206f6ad779121af502b1e49b8e
ryym <ryym.64@gmail.com>
2017-04-13 22:27:23 +0900

    feat(rend/load): Add loading bars

---
+++ renderer/components/shared/LoadingBars/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+export default function LoadingBars() {
+  return (
+    <div className="loading">
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
