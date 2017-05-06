0f3a9c052a00c27a19b11af7e0e326d3c3537016
ryym <ryym.64@gmail.com>
2017-05-06 10:40:47 +0900

    refac(rend/redux): Rewrite redux utilities in TS

--- renderer/redux/readable-store.ts
+++ renderer/redux/readable-store.ts
@@ -1,4 +1,6 @@
 import bindMethodContext from '../lib/utils/bind-method-context';
+import { Store, Reducer, Unsubscribe } from 'redux'
+import { Action } from './actions'
 
 /**
  * ReadableStore provides same API with Redux Store.
@@ -17,31 +19,38 @@ import bindMethodContext from '../lib/utils/bind-method-context';
  * we decide to just wrap a store in new class because
  * an enhanced store couldn't be statically typed.
  */
-class ReadableStore {
-  constructor(store, createReader) {
+class ReadableStore<S, R> {
+  private readonly store:  Store<S>
+  private readonly createReader: (state: S) => R
+  private _shouldUpdateReader: boolean
+  private _activated: boolean;
+  private _reader: R
+
+  constructor(store: Store<S>, createReader: (state: S) => R) {
     this.store = store;
     this.createReader = createReader;
     this._shouldUpdateReader = true;
+    this._activated = false;
     bindMethodContext(this);
   }
 
-  getState() {
+  getState(): S {
     return this.store.getState();
   }
 
-  dispatch(action) {
-    return this.store.dispatch(action);
+  dispatch(action: Action<any>) {
+    this.store.dispatch(action);
   }
 
-  subscribe(listener) {
+  subscribe(listener: () => void): Unsubscribe {
     return this.store.subscribe(listener);
   }
 
-  replaceReducer(nextReducer) {
-    return this.replaceReducer(nextReducer);
+  replaceReducer(nextReducer: Reducer<S>) {
+    this.replaceReducer(nextReducer);
   }
 
-  getReader() {
+  getReader(): R {
     if (this._shouldUpdateReader) {
       this._reader = this.createReader(this.store.getState());
       this._shouldUpdateReader = false;
@@ -49,14 +58,19 @@ class ReadableStore {
     return this._reader;
   }
 
-  activate() {
-    this.subscribe(() => {
-      this._shouldUpdateReader = true;
-    });
+  activate(): ReadableStore<S, R> {
+    if (! this._activated) {
+      this.subscribe(() => {
+        this._shouldUpdateReader = true;
+      });
+    }
     return this;
   }
 }
 
-export default function createReadableStore(store, createReader) {
+export default function createReadableStore<S, R>(
+  store: Store<S>,
+  createReader: (state: S) => R
+): ReadableStore<S, R> {
   return new ReadableStore(store, createReader).activate();
 }
