e070e9b329e36899484858d51962e788155377fe
ryym <ryym.64@gmail.com>
2017-11-02 16:57:52 +0900

    feat(api): Add lib to extract link URLs

--- renderer/lib/github-api/lib.ts
+++ renderer/lib/github-api/lib.ts
@@ -1,7 +1,29 @@
+import { PageLinks, PageRel } from './types';
+
 export const paramsToQuery = (params: { [key: string]: any }) => {
   const query = Object.keys(params).reduce((q, k) => {
     const v = params[k];
     return v != null ? `${q}&${k}=${v}` : `${q}&${k}`;
   }, '');
   return query.length > 0 ? `?${query}` : query;
+};
+
+// GitHub provides pagination URLs as a string such as:
+// <https://api.github.com/notifications?page=3>; rel="next", <https://api.github.com/notifications?page=7>; rel="last"
+export const extractLinks = (link: string): PageLinks => {
+  const links: PageLinks = {};
+  link.split(',').forEach((part: string) => {
+    const m = part.match(/<([^>]+)>;\s+rel="(first|prev|next|last)"/);
+    if (m != null) {
+      const url = m[1];
+      const rel = m[2] as PageRel;
+      links[rel] = url;
+    }
+  });
+  return links;
+};
+
+export const extractPage = (url: string): number | null => {
+  const m = url.match(/(?:\?|&)page=(\d+)/);
+  return m == null ? null : Number(m[1]);
 };
--- renderer/lib/github-api/types.ts
+++ renderer/lib/github-api/types.ts
@@ -7,3 +7,12 @@ export interface GitHubAPI {
   request(path: string, options?: FetchOptions): Promise<Response>;
   requestSoon(path: string, options?: FetchOptions): Promise<Response>;
 }
+
+export type PageLinks = {
+  first?: string,
+  prev?: string,
+  next?: string,
+  last?: string,
+};
+
+export type PageRel = keyof PageLinks;
